105f625f07a04ae16093ec9fd0edc711e053baba
OskaKim <o2kim0225@naver.com>
2022-01-02 17:01:54 +0900

    impl - 타일 기본 클릭처리

--- Assets/Scripts/TilemapTouchHandler.cs
+++ Assets/Scripts/TilemapTouchHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,37 @@ using UnityEngine.Tilemaps;
 
 public class TilemapTouchHandler : MonoBehaviour
 {
-    [SerializeField] Tilemap tilemap;
-
+    // NOTE : 터치용 타일맵.
+    // 타일이 여러 개가 존재할 수 있지만 gridPosition은 동일하기 때문에 터치 조작용 타일은 하나로 통일
+    [SerializeField] TilemapCollider2D iputTouchTilemapCollider;
+    [SerializeField] Tilemap inputTouchTilemap;
+    [SerializeField] Tilemap changeTouchTile;
+    
+    // TODO : unirx를 적용해서 인풋 이벤트로 작성
     private void Update()
     {
-        // todo : test get tile
-        if (Input.GetKeyDown(KeyCode.F))
+        try
         {
-            var clickedPosition = new Vector3Int(0, -1, 0);
-            tilemap.SetTile(clickedPosition, null);
+            if (Input.GetMouseButtonDown(0))
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue, 3.5f);
+                var hit = Physics2D.Raycast(ray.origin, Vector3.zero);
+                
+                if (hit.collider == iputTouchTilemapCollider)
+                {
+                    var worldPosition = ray.GetPoint(-ray.origin.z / ray.direction.z);
+                    var gridPosition = inputTouchTilemap.WorldToCell(worldPosition);
+
+                    Debug.Log($"TouchedPosition:{gridPosition}");
+                    
+                    // test : get tile
+                    changeTouchTile.SetTile(gridPosition, null);
+                }
+            }
+        }
+        catch (NullReferenceException)
+        {
         }
     }
 }
