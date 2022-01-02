cffdfb888881d75942c62fd4c8f441c5e9eee9e9
OskaKim <o2kim0225@naver.com>
2022-01-02 17:16:42 +0900

    impl - TilemapTouchHandler

--- Assets/Scripts/TilemapTouchHandler.cs
+++ Assets/Scripts/TilemapTouchHandler.cs
@@ -8,10 +8,11 @@ public class TilemapTouchHandler : MonoBehaviour
 {
     // NOTE : 터치용 타일맵.
     // 타일이 여러 개가 존재할 수 있지만 gridPosition은 동일하기 때문에 터치 조작용 타일은 하나로 통일
-    [SerializeField] TilemapCollider2D iputTouchTilemapCollider;
-    [SerializeField] Tilemap inputTouchTilemap;
-    [SerializeField] Tilemap changeTouchTile;
-    
+    [SerializeField] private TilemapCollider2D iputTouchTilemapCollider;
+    [SerializeField] private Tilemap inputTouchTilemap;
+
+    public static Action<Vector3Int> TouchTileAction;
+
     // TODO : unirx를 적용해서 인풋 이벤트로 작성
     private void Update()
     {
@@ -22,16 +23,16 @@ public class TilemapTouchHandler : MonoBehaviour
                 var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue, 3.5f);
                 var hit = Physics2D.Raycast(ray.origin, Vector3.zero);
-                
+
                 if (hit.collider == iputTouchTilemapCollider)
                 {
                     var worldPosition = ray.GetPoint(-ray.origin.z / ray.direction.z);
                     var gridPosition = inputTouchTilemap.WorldToCell(worldPosition);
-
-                    Debug.Log($"TouchedPosition:{gridPosition}");
                     
-                    // test : get tile
-                    changeTouchTile.SetTile(gridPosition, null);
+                    if (TouchTileAction != null)
+                    {
+                        TouchTileAction.Invoke(gridPosition);
+                    }
                 }
             }
         }
