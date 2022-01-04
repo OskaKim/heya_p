74f6ada2bf0660304528e7e2b103e810081beca3
OskaKim <o2kim0225@naver.com>
2022-01-04 22:31:43 +0900

    fix - 타일 터치 이벤트 로직을 unirx기반으로 하기

--- Assets/Scripts/TilemapTouchHandler.cs
+++ Assets/Scripts/TilemapTouchHandler.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -11,33 +13,22 @@ public class TilemapTouchHandler : MonoBehaviour
     [SerializeField] private TilemapCollider2D iputTouchTilemapCollider;
     [SerializeField] private Tilemap inputTouchTilemap;
 
-    public static Action<Vector3Int> TouchTileAction;
-
-    // TODO : unirx를 적용해서 인풋 이벤트로 작성
-    private void Update()
+    private Subject<Vector3Int> onTouchTilemap = new Subject<Vector3Int>();
+    public IObservable<Vector3Int> OnTouchTilemap { get => onTouchTilemap; }
+    private void Start() =>
+    this.UpdateAsObservable()
+    .Where(_ => Input.GetMouseButtonDown(0))
+    .Select(_ => Camera.main.ScreenPointToRay(Input.mousePosition))
+    .Where(ray =>
     {
-        try
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue, 3.5f);
-                var hit = Physics2D.Raycast(ray.origin, Vector3.zero);
-
-                if (hit.collider == iputTouchTilemapCollider)
-                {
-                    var worldPosition = ray.GetPoint(-ray.origin.z / ray.direction.z);
-                    var gridPosition = inputTouchTilemap.WorldToCell(worldPosition);
-                    
-                    if (TouchTileAction != null)
-                    {
-                        TouchTileAction.Invoke(gridPosition);
-                    }
-                }
-            }
-        }
-        catch (NullReferenceException)
-        {
-        }
-    }
+        var hit = Physics2D.Raycast(ray.origin, Vector3.zero);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue, 3.5f);
+        return hit.collider == iputTouchTilemapCollider;
+    })
+    .Subscribe(ray =>
+    {
+        var worldPosition = ray.GetPoint(-ray.origin.z / ray.direction.z);
+        var gridPosition = inputTouchTilemap.WorldToCell(worldPosition);
+        onTouchTilemap.OnNext(gridPosition);
+    });
 }
--- Assets/Scripts/TouchFurnitureTest.cs
+++ Assets/Scripts/TouchFurnitureTest.cs
@@ -1,30 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TouchFurnitureTest : MonoBehaviour
 {
+    [SerializeField] private TilemapTouchHandler tilemapTouchHandler;
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private TileBase tile1;
     [SerializeField] private TileBase tile2;
     [SerializeField] private TileBase tile3;
     private TileBase currentTile;
-    private void OnEnable()
+    private void Start()
     {
-        TilemapTouchHandler.TouchTileAction += TouchTile;
+        tilemapTouchHandler.OnTouchTilemap.Subscribe(touchedPosition => TouchTile(touchedPosition));
     }
-    private void OnDisable()
+    private void Update()
     {
-        TilemapTouchHandler.TouchTileAction -= TouchTile;
+        if (Input.GetKeyDown(KeyCode.Q)) currentTile = tile1;
+        if (Input.GetKeyDown(KeyCode.W)) currentTile = tile2;
+        if (Input.GetKeyDown(KeyCode.E)) currentTile = tile3;
+        if (Input.GetKeyDown(KeyCode.R)) currentTile = null;
     }
-    private void Update() {
-        if(Input.GetKeyDown(KeyCode.Q)) currentTile = tile1;
-        if(Input.GetKeyDown(KeyCode.W)) currentTile = tile2;
-        if(Input.GetKeyDown(KeyCode.E)) currentTile = tile3;
-        if(Input.GetKeyDown(KeyCode.R)) currentTile = null; 
-    }
-    public void TouchTile(Vector3Int touchedPosition)
+    private void TouchTile(Vector3Int touchedPosition)
     {
         tilemap.SetTile(touchedPosition, currentTile);
     }
