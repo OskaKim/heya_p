0f7bd5ecef07e3282fde2469f61b6680e1fae624
OskaKim <o2kim0225@naver.com>
2022-01-09 18:42:47 +0900

    test - game starter에서 가구를 배치하도록

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 // NOTE : 외부 데이터를 바탕으로 게임을 구성하는 클래스
 public class GameStarter : MonoBehaviour {
@@ -9,9 +10,16 @@ public class GameStarter : MonoBehaviour {
 
     [SerializeField] Grid grid;
     [SerializeField] GameObject characterPrefab;
+    [SerializeField] Tilemap furnitureTilemap;
+    [SerializeField] TileBase[] furnitures;
 
     private void Start() {
         var worldPos = grid.CellToWorld(characterInitializePosition);
         GameObject.Instantiate(characterPrefab, worldPos, Quaternion.identity);
+        
+        // todo : 외부 데이터에서 가구를 배치하도록
+        furnitureTilemap.SetTile(new Vector3Int(2,0,0), furnitures[2]);
+        furnitureTilemap.SetTile(new Vector3Int(1,4,0), furnitures[1]);
+        furnitureTilemap.SetTile(new Vector3Int(-1,-1,0), furnitures[0]);
     }
 }
