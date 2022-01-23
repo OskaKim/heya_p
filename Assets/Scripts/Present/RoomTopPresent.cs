af681029f3e30ee1fb0e49e1c3b9cc3257da9bd5
OskaKim <o2kim0225@naver.com>
2022-01-23 13:29:21 +0900

    present준비

---
+++ Assets/Scripts/Present/RoomTopPresent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTopPresent : MonoBehaviour
+{
+    [SerializeField] private UIInstallFurnitureView uiInstallFurnitureView;
+
+    private void Start()
+    {
+        
+    }
+}
