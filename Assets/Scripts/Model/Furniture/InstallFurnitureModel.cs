4734348a9578fcd8eda60afb658a1723cad5d2fd
OskaKim <o2kim0225@naver.com>
2022-01-25 22:21:59 +0900

    impl - 선택한 타일이 설치되도록 하기

--- Assets/Scripts/Model/Furniture/InstallFurnitureModel.cs
+++ Assets/Scripts/Model/Furniture/InstallFurnitureModel.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Tilemaps;
 using System.Collections.Generic;
+using System.Linq;
+using DataBase;
 
 public class InstallFurnitureModel
 {
     public ReactiveProperty<int> SelectedFurniture { get; set; } = new ReactiveProperty<int>(-1);
-    public ReactiveProperty<bool> FurnitureInstallModeToggle { get; set; } = new ReactiveProperty<bool>(false);
-    public List<DataBase.FurnitureData> FurnitureDataBase
+    public List<FurnitureData> FurnitureDataBase
     {
         get
         {
-            return DataBase.Database.FurnitureDatabase;
+            return MasterData.FurnitureDatabase;
         }
+    }
+    private FurnitureData GetFurnitureData(int id)
+    {
+        return FurnitureDataBase.FirstOrDefault(x => x.id == id);
+    }
+    public TileBase GetFurnitureTile(int id)
+    {
+        return GetFurnitureData(id).tile;
     }
 }
