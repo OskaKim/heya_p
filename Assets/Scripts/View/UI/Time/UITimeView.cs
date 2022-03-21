a8c5dfdca09af48700c8d8ad95652057ad97d4c6
OskaKim <o2kim0225@naver.com>
2022-03-21 17:21:13 +0900

    ui time view에 시간 텍스트를 표시

--- Assets/Scripts/View/UI/Time/UITimeView.cs
+++ Assets/Scripts/View/UI/Time/UITimeView.cs
@@ -8,11 +8,13 @@ namespace timeinfo
     public class UITimeView : MonoBehaviour
     {
         [SerializeField] private TMP_Text dayText;
+        [SerializeField] private TMP_Text timeText;
 
         public void UpdateTime(DateTime updatedTime)
         {
             //Debug.Log(updatedTime.ToString("MM/dd/yyyy HH:mm:ss", DateTimeFormatInfo.InvariantInfo));
-            //dayText.text = 
+            dayText.text = updatedTime.ToString("MM/dd", DateTimeFormatInfo.InvariantInfo);
+            timeText.text = updatedTime.ToString("HH:mm", DateTimeFormatInfo.InvariantInfo);
         }
     }
 }
