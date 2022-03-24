98a222fb8a8f8149839c2ee49b892c8811d994a2
OskaKim <o2kim0225@naver.com>
2022-03-24 21:08:47 +0900

    impl - 시간 재생, 일시정지 버튼 추가

--- Assets/Scripts/View/UI/Time/UITimeView.cs
+++ Assets/Scripts/View/UI/Time/UITimeView.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using System;
 using System.Globalization;
@@ -9,12 +10,24 @@ namespace timeinfo
     {
         [SerializeField] private TMP_Text dayText;
         [SerializeField] private TMP_Text timeText;
+        [SerializeField] private Button playPauseButton;
+        [SerializeField] private Text playPauseButtonText;
+
+        public event Action<bool> OnPlayPauseButtonClicked;
+        private bool isPlaying = true;
 
         public void UpdateTime(DateTime updatedTime)
         {
             //Debug.Log(updatedTime.ToString("MM/dd/yyyy HH:mm:ss", DateTimeFormatInfo.InvariantInfo));
             dayText.text = updatedTime.ToString("MM/dd", DateTimeFormatInfo.InvariantInfo);
             timeText.text = updatedTime.ToString("HH:mm", DateTimeFormatInfo.InvariantInfo);
+        }
+
+        public void OnClickedButton()
+        {
+            isPlaying = !isPlaying;
+            playPauseButtonText.text = isPlaying ? "Play" : "Pause";
+            OnPlayPauseButtonClicked?.Invoke(isPlaying);
         }
     }
 }
