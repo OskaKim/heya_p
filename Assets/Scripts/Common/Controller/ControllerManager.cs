f109149a096deca0dbbddb2bd606e2b06793c159
OskaKim <o2kim0225@naver.com>
2022-03-24 22:35:18 +0900

    controller manager의 구현 내용을 빈 함수로 작성

--- Assets/Scripts/Common/Controller/ControllerManager.cs
+++ Assets/Scripts/Common/Controller/ControllerManager.cs
@@ -1,9 +1,39 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace common
 {
     public class ControllerManager : common.Singleton<MonoBehaviour>
     {
-        
+        public enum ControllerType
+        {
+            Character,
+            Furniture,
+            FurnitureScroillView,
+            GridInstall,
+            TimeInfo
+        };
+        private List<BaseController> runningControllers;
+        private List<BaseController> pausedControllers;
+
+        protected sealed override void Awake()
+        {
+            runningControllers = new List<BaseController>();
+        }
+
+        public void RunController(ControllerType controllerType)
+        {
+            // todo : controller실행
+        }
+
+        public void PauseController(ControllerType controllerType)
+        {
+            // todo : controller일시정지
+        }
+
+        public void StopController(ControllerType controllerType)
+        {
+            // todo : controller정지(삭제)
+        }
     }
 }
