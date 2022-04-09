fbad90fd1b52a10b04e6a96836d7cbad0b2f432e
OskaKim <o2kim0225@naver.com>
2022-04-09 18:02:34 +0900

    필요 크기만큼 그리드 라인을 생성

--- Assets/Scripts/View/Grid/GridLineDrawerView.cs
+++ Assets/Scripts/View/Grid/GridLineDrawerView.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace grid
 {
@@ -7,10 +8,44 @@ namespace grid
     {
         [SerializeField] private GameObject GridLinePrefab;
 
-        public void DrawGridLine(List<GameObject> targetTiles)
+        private List<GameObject> gridLineCaches = new List<GameObject>();
+        public void DrawGridLine(List<Vector3> targetTilePositions)
         {
-            // todo : 해당하는 타일에 그리드 라인을 그리기
-            Debug.Log(targetTiles);
+            if (gridLineCaches.Count < targetTilePositions.Count)
+            {
+                // todo : 재활용 가능한 구조로 개수
+                foreach (var gridLineCache in gridLineCaches)
+                {
+                    GameObject.DestroyImmediate(gridLineCache);
+                }
+                gridLineCaches.Clear();
+                gridLineCaches = new List<GameObject>(targetTilePositions.Count);
+                for (int i = 0; i < targetTilePositions.Count; ++i)
+                {
+                    // note : 생성
+                    var gridLine = new GameObject($"GridLine{i}");
+                    gridLine.transform.SetParent(transform);
+                    gridLineCaches.Add(gridLine);
+                }
+            }
+            else
+            {
+                // note : 이미 있으므로 표시
+                foreach (var gridLineCache in gridLineCaches)
+                {
+                    gridLineCache.SetActive(true);
+                }
+            }
+
+            // todo : 해당하는 타일 위치에 그리드 라인을 그리기
+        }
+
+        public void HideGridLine()
+        {
+            foreach (var gridLineCache in gridLineCaches)
+            {
+                gridLineCache.SetActive(false);
+            }
         }
     }
 }
